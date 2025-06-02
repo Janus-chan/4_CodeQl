989b20b281c8783d89d9673fda0d65752f251718
Januson <januson.john@comorin.co>
2025-06-02 15:15:08 +0530

    code check 2

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ app.post('/login', (req, res) => {
     
     const { username, password } = req.body;
     let mPassword = maskPassword(password);
-    // codeql[js/clear-text-logging]: This is intentionally masked password
     console.log('Received login attempt:', username, mPassword);
     const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
     // This is vulnerable to SQL injection
